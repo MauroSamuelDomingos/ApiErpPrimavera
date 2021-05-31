d33551f8cf3b514d2efa159005ff5db08068580a
MauroSamuelDomingos <65758587+MauroSamuelDomingos@users.noreply.github.com>
2021-05-31 11:07:39 +0100

    Delete PortalApi directory

--- PortalApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PortalApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- PortalApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PortalApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
